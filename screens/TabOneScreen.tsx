52b52bb4731fb42003f8f50de4deb70337e05282
Zachary A Kniffen <zacharyakniffen@gmail.com>
2021-03-13 16:44:29 -0600

    added react-native-modal-dropdown and putin json data with javascript, but I'm going to try it with useState in React next

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,39 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import data from "../lean.json";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { useState } from 'react';
+
 
 export default function TabOneScreen() {
+let myList:string[] = [];
+for (let item of data){
+  myList.push(item.name);
+}
   return (
     <View style={styles.container}>
+<ModalDropdown style={styles.dropdown} textStyle={styles.dropdownText} options={myList}/>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />*/}
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
+  dropdown:{
+    backgroundColor: "red",
+    width: "75%",
+    height: "6%",
+    borderRadius: 20,
+    
+  },
+  dropdownText:{
+    
+  },
   container: {
     flex: 1,
     alignItems: 'center',

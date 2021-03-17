0048c791998d812ca9ba5b248c771849d7c4a1c8
Zachary A Kniffen <zacharyakniffen@gmail.com>
2021-03-16 20:26:33 -0500

    I got the child component selection of dropdown to show under the selector, and now I just need to make an array for the useState on the selected items so I can show more than one and calculate them together, plus refactor then handle change to account for the greens portion.

---
+++ components/ModalDropdown.js
@@ -0,0 +1,70 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+import Colors from '../constants/Colors';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+export default function ModalDropdownFunction({options, handleOnChange, selected}) {
+  
+const handleChange = (e) => {
+ 
+  handleOnChange(options[e].name);
+}
+
+  return (
+    <View>
+<ModalDropdown  onSelect={handleChange} options={options.map(options => options.name)}  style={styles.selectBox} dropdownStyle={styles.dropdown} textStyle={styles.selectBoxText} dropdownTextStyle={styles.dropdownText} />
+    <Text style={styles.myText} >{selected}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  myText:{
+    fontSize: 40
+  },
+  selectBox:{
+    backgroundColor: "red",
+    alignSelf: "center",
+
+    borderRadius: 20,
+    
+    alignItems: 'center',
+    
+  },
+  selectBoxText:{
+    fontSize: 30,
+
+  },
+  dropdown:{
+    
+    width: "50%",
+    marginHorizontal: 0,
+    paddingHorizontal: 0,
+    alignItems: "stretch"
+  },
+
+  dropdownText:{
+    fontSize: 20,
+    alignItems: "stretch",
+    minWidth: 100
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    /*justifyContent: 'center',*/
+    backgroundColor: "blue",
+  
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  });
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,48 +1,69 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import data from "../lean.json";
+import leanData from "../lean.json";
+import greenData from "../lean.json";
+
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import ModalDropdown from '../components/ModalDropdown';
 import { Text, View } from '../components/Themed';
-import ModalDropdown from 'react-native-modal-dropdown';
+
 import { useState } from 'react';
+import { loadOptions } from '@babel/core';
 
 
 export default function TabOneScreen() {
-let myList:string[] = [];
-for (let item of data){
-  myList.push(item.name);
-}
+
+const [ leanList, setLeanList ] = useState(leanData);
+const [ greenList, setGreenList ] = useState(greenData);
+const [ selected, setSelected ] = useState();
+const [ greens, setGreens ] = useState("");
+
+
+const handleOnChange = (name: React.SetStateAction<any>) =>{
+  setSelected(name);
+}  
+const handleOnGreens = (name: React.SetStateAction<any>) =>{
+  setGreens(name);
+}  
+
   return (
+    
     <View style={styles.container}>
-<ModalDropdown style={styles.dropdown} textStyle={styles.dropdownText} options={myList}/>
+      <ModalDropdown selected={selected} handleOnChange={handleOnChange} style={styles.topDrop} options={leanList}/>
+      <ModalDropdown greens={greens} handleOnGreens={handleOnGreens} style={styles.bottomDrop} options={greenList}/>
       <Text style={styles.title}>Tab One</Text>
       {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />*/}
+      
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  dropdown:{
-    backgroundColor: "red",
-    width: "75%",
-    height: "6%",
-    borderRadius: 20,
-    
-  },
-  dropdownText:{
-    
-  },
+divStyle:{
+  backgroundColor:"blue"
+},
+ topDrop:{
+  top: "20%",
+  color:"blue"
+ },
+ bottomDrop:{
+  bottom:"20%"
+ },
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "blue"
+    justifyContent: "space-around",
+    backgroundColor: "blue",
+  
   },
   title: {
+    display:"flex",
     fontSize: 20,
     fontWeight: 'bold',
+    flexDirection: "column",
+    alignSelf: "flex-start"
   },
   separator: {
     marginVertical: 30,
